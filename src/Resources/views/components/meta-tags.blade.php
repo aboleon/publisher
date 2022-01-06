882213d00f5a28e48a1b935f31d5f909eef0bb70
Andrian Mihailov <amihailov@wagaia.com>
2022-01-06 03:55:33 +0100

    Optimizations

--- src/Resources/views/components/meta-tags.blade.php
+++ src/Resources/views/components/meta-tags.blade.php
@@ -1,8 +1,8 @@
 @section('meta-tags')
-    <title>{{ $content->m_title ?: config('app.name') }}</title>
-    <meta name="description" content="{{ $content->m_desc ?: $content->abstract }}">
+    <title>{!! ($content->m_title ?: $content->title) . ' :: '. config('app.name') !!}</title>
+    <meta name="description" content="{!! $content->m_desc ?: $content->abstract  !!}">
     <!-- open graph -->
-    <meta property="og:title" content="{{ $content->m_title }}"/>
+    <meta property="og:title" content="{!! ($content->m_title ?: $content->title) . ' :: '. config('app.name') !!}"/>
     <meta property="og:locale" content="{{ app()->getLocale() }}_{{ strtoupper(app()->getLocale()) }}"/>
     <meta property="og:description" content="{{ $content->m_desc ?: $content->abstract }}">
     <meta property="og:url" content="{{ config('app.url') }}"/>
